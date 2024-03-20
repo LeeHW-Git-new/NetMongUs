a61de57a08fa390a57b146a744706a6086fa6f2a
Lee <aadd306@gmail.com>
2024-03-20 15:26:50 +0900

    The Skeld 스프라이트 임포트 , 맵 스프라이트+ 애니메이션 배치 ing

---
+++ NetMongUs/Assets/Map/TheSkeld/Scripts/WeaponPipLight.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponPipLight : MonoBehaviour
+{
+    Animator animator;
+
+    WaitForSeconds wait = new WaitForSeconds(0.15f);
+
+    List<WeaponPipLight> lights = new List<WeaponPipLight>();
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            var child = transform.GetChild(i).GetComponent<WeaponPipLight>();
+            if(child)
+            {
+                lights.Add(child);
+            }
+        }
+    }
+
+    public void TurnOnLight() 
+    {
+        animator.SetTrigger("on");
+        StartCoroutine(TurnOnLightAtChild());
+    }
+
+    IEnumerator TurnOnLightAtChild()
+    {
+        yield return wait;
+
+        foreach(var child in lights)
+        {
+            child.TurnOnLight();
+        }
+    }
+}
---
+++ NetMongUs/Assets/Map/TheSkeld/Scripts/WeaponPipLightStarter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponPipLightStarter : MonoBehaviour
+{
+    WaitForSeconds wait = new WaitForSeconds(1f);
+
+    List<WeaponPipLight> lights = new List<WeaponPipLight>();
+
+
+    void Start()
+    {
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            var child = transform.GetChild(i).GetComponent<WeaponPipLight>();
+            if(child)
+            {
+                lights.Add(child);
+            }
+        }
+        StartCoroutine(TrunOnPipeLight());
+    }
+
+
+    IEnumerator TrunOnPipeLight()
+    {
+        while(true)
+        {
+            yield return wait;
+
+            foreach(var child in lights)
+            {
+                child.TurnOnLight();
+            }
+        }
+    }
+}
